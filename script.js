352a8e0eaf4e3e516deae652ffec38f38df47932
RajithaPulasthi <rajithapulasthi11@gmail.com>
2023-11-07 10:59:58 +0530

    uddated JS and html files

--- script.js
+++ script.js
@@ -4,37 +4,59 @@ let dice = document.querySelectorAll("img");
 let currentPlayer = 1;
 let scorePlayer1 = 0;
 let scorePlayer2 = 0;
+let round = 0;
 
 function roll() {
-    dice.forEach(function (die) {
-        die.classList.add("shake");
-    });
 
-    setTimeout(function () {
+    // Check the round count and if it's over, runs the following script
+    if (round >= 1) {
+        // Show the winner status and the draw status
+        document.querySelector("#total").innerHTML = (scorePlayer1 !== scorePlayer2) ? (scorePlayer1 > scorePlayer2) ? "The winner is Player 1" : "The winner is Player 2" : "Game DRAW"
+        console.log("THIS SHOULD STOP");
+        round = 0; // reset the round
+        scorePlayer1 = 0; // reset player scores
+        scorePlayer2 = 0;
+        return
+    } else {
+
+        console.log(round)
+
         dice.forEach(function (die) {
-            die.classList.remove("shake");
+            die.classList.add("shake");
         });
 
-        let dieOneValue = Math.floor(Math.random() * 6);
-        let dieTwoValue = Math.floor(Math.random() * 6);
-        console.log(dieOneValue, dieTwoValue);
+        setTimeout(function () {
+            dice.forEach(function (die) {
+                die.classList.remove("shake");
+            });
 
-        document.querySelector("#die-1").setAttribute("src", images[dieOneValue]);
-        document.querySelector("#die-2").setAttribute("src", images[dieTwoValue]);
-        document.querySelector("#total").innerHTML = "Your Value is " + ((dieOneValue + 1) + (dieTwoValue + 1));
 
-        if (currentPlayer === 1) {
-            scorePlayer1 += (dieOneValue + 1) + (dieTwoValue + 1);
-            document.querySelector("#score-player1").textContent = scorePlayer1; // Display Player 1's score
-        } else {
-            scorePlayer2 += (dieOneValue + 1) + (dieTwoValue + 1);
-            document.querySelector("#score-player2").textContent = scorePlayer2; // Display Player 2's score
-        }
+            let dieOneValue = Math.floor(Math.random() * 6); // Dice one value
+            let dieTwoValue = Math.floor(Math.random() * 6); // Dice two value
+            // console.log(dieOneValue, dieTwoValue);
 
-        if (currentPlayer === 1) {
-            currentPlayer = 2;
-        } else {
-            currentPlayer = 1;
-        }
-    }, 1000);
+            document.querySelector("#die-1").setAttribute("src", images[dieOneValue]); // change the image according to the value
+            document.querySelector("#die-2").setAttribute("src", images[dieTwoValue]); // change the image according to the value
+            document.querySelector("#total").innerHTML = "Your Value is " + ((dieOneValue + 1) + (dieTwoValue + 1));
+
+            if (currentPlayer === 1) {
+                scorePlayer1 += (dieOneValue + 1) + (dieTwoValue + 1); // Sum of 2 dices
+                document.querySelector("#score-player1").textContent = scorePlayer1; // Display Player 1's score
+                round = round + 0.5 // half of a round completed by player one
+            } else {
+                scorePlayer2 += (dieOneValue + 1) + (dieTwoValue + 1);
+                document.querySelector("#score-player2").textContent = scorePlayer2; // Display Player 2's score
+                round = round + 0.5 // half of a round completed by one player two
+            }
+
+            if (currentPlayer === 1) {
+                currentPlayer = 2;
+            } else {
+                currentPlayer = 1;
+            }
+
+        }, 1000);
+    }
+
 }
+
